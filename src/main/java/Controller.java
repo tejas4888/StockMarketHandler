1eb2201dc4889c00eeec8f9aecde947b9e7946d0
Tejas Chheda <tejas4888@gmail.com>
2017-10-16 17:16:30 +0530

    Test Commit #1
    Added comment from web

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -12,6 +12,7 @@ public class Controller extends Application{
     {
         System.out.println("Hello");
         launch(args);
+        //Testing123
     }
 
     public void start(Stage primaryStage) throws Exception
