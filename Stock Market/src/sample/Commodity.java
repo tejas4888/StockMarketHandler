c2dec78e8ddb1daf0344a00400b6e0d674949318
hussain1605 <hussain1605@gmail.com>
2017-10-21 15:42:50 +0530

    Add files via upload

---
+++ Stock Market/src/sample/Commodity.java
@@ -0,0 +1,42 @@
+package sample;
+
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class Commodity {
+    private final SimpleStringProperty stock_name;
+    private final SimpleDoubleProperty stock_last;
+    private final  SimpleDoubleProperty stock_high;
+    private final  SimpleDoubleProperty stock_low;
+    private final  SimpleDoubleProperty stock_change;
+
+    public Commodity(String stock_name, Double stock_last, Double stock_high, Double stock_low, Double stock_change) {
+        this.stock_name = new SimpleStringProperty(stock_name);
+        this.stock_last = new SimpleDoubleProperty(stock_last);
+        this.stock_high = new SimpleDoubleProperty(stock_high);
+        this.stock_low = new SimpleDoubleProperty(stock_low);
+        this.stock_change = new SimpleDoubleProperty(stock_change);
+    }
+
+    public String getStock_name() {
+        return stock_name.get();
+    }
+
+    public Double getStock_last() {
+        return stock_last.get();
+    }
+
+    public Double getStock_high() {
+        return stock_high.get();
+    }
+
+    public Double getStock_low() {
+        return stock_low.get();
+    }
+
+    public Double getStock_change() {
+        return stock_change.get();
+    }
+}
